a9955ef61f80ead0c39f236175c6d85d02a33513
Roland Leißa <leissa@cs.uni-saarland.de>
2015-02-17 20:40:07 +0100

    polished setup script

---
+++ setup.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+THREADS=2
+CUR=`pwd`
+BRANCH=master
+
+# fetch sources
+wget http://llvm.org/releases/3.4.2/llvm-3.4.2.src.tar.gz
+tar xf llvm-3.4.2.src.tar.gz
+rm llvm-3.4.2.src.tar.gz
+mv llvm-3.4.2.src llvm
+cd llvm/tools
+wget http://llvm.org/releases/3.4.2/cfe-3.4.2.src.tar.gz
+tar xf cfe-3.4.2.src.tar.gz
+rm cfe-3.4.2.src.tar.gz
+mv cfe-3.4.2.src clang
+cd "${CUR}"
+git clone git@github.com:AnyDSL/thorin.git -b ${BRANCH}
+git clone git@github.com:AnyDSL/impala.git -b ${BRANCH}
+
+# create build/install dirs
+mkdir -p llvm_build
+mkdir -p llvm_install
+mkdir -p thorin/build
+mkdir -p impala/build
+
+# build llvm
+cd llvm_build
+cmake ../llvm -DLLVM_REQUIRES_RTTI=true -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX="${CUR}/llvm_install"
+make install -j${THREADS}
+
+# build thorin
+cd "${CUR}/thorin/build"
+cmake .. -DCMAKE_BUILD_TYPE=Debug -DLLVM_DIR="${CUR}/llvm_install/share/llvm/cmake"
+make -j${THREADS}
+
+# build impala
+cd "${CUR}/impala/build"
+cmake .. -DCMAKE_BUILD_TYPE=Debug -DLLVM_DIR="${CUR}/llvm_install/share/llvm/cmake" -DTHORIN_DIR="${CUR}/thorin"
+make -j${THREADS}
+export PATH="${CUR}/llvm_install/bin:${CUR}/impala/build/bin:$PATH"
+
+# go back to current dir
+cd "${CUR}"
+
+echo
+echo "Put the following line into your '~/.bashrc' in order to have 'impala' and 'clang' in your path:"
+echo "export PATH=${CUR}/llvm_install/bin:${CUR}/impala/build/bin:\$PATH"
+echo "WARNING: Note that this will override any system installation of llvm/clang."

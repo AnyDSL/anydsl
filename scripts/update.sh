cb5a08f1fd46edf1f23f9c39babaeb4685587f94
Roland Leißa <leissa@cs.uni-saarland.de>
2015-06-26 15:27:26 +0200

    use update.sh also as standalone script

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 set -eu
 
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-cd "$DIR"
-cd ..
-cd "$1"
+if [[ "$#" -ne 1 && "$#" -ne 2 ]]; then
+    echo "usage: $0 (thorin | impala) [commit_sha1]" >& 2
+    exit 1
+fi
+
+
+HERE=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd "$HERE"/../"$1"
 
 echo "Executing doxygen on $1"
 doxygen doxyfile
@@ -14,6 +18,12 @@ git pull
 mkdir -p doxygen/"$1"
 cp -R ../"$1"/html/* doxygen/"$1"/
 
+if [ -z ${2-} ]; then
+    MESSAGE="bumping doxygen for AnyDSL/$1"
+else
+    MESSAGE="updating doxygen for commit AnyDSL/$1@$2"
+fi
+
 git add doxygen/"$1"/*
-git commit -m "updating doxygen for commit AnyDSL/$1@$2"
+git commit -m "$MESSAGE"
 git push

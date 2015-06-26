9baf374ee5e448ff1d7066479521414a1146d9e0
Roland Leißa <leissa@cs.uni-saarland.de>
2015-06-26 15:27:11 +0200

    retab + cleanup

--- scripts/pre-push.sh
+++ scripts/pre-push.sh
@@ -6,28 +6,23 @@ revert=false
 
 while read local_ref local_sha remote_ref remote_sha
 do
-	remote_branch=${remote_ref#refs/heads/}
-	if [ "$remote_ref" != "$remote_branch" ]; then
-		if [ "$remote_branch" == "master" ]; then
-			if [ "$local_branch" != "master" ]; then
-				git checkout master
-				revert=true
-			fi
+    remote_branch=${remote_ref#refs/heads/}
+    if [ "$remote_ref" != "$remote_branch" ]; then
+        if [ "$remote_branch" == "master" ]; then
+            if [ "$local_branch" != "master" ]; then
+                git checkout master
+                revert=true
+            fi
 
-			changes="$(git log origin/master..master)"
-			if [ -z "$changes" ]; then
-				continue;
-			fi
-
-			
-    			DIR="$1"
-    			SHA1="$(git rev-parse HEAD)"
-
-    			../scripts/update.sh "$DIR" "$SHA1"
-		fi
-	fi
+            changes="$(git log origin/master..master)"
+            if [ -z "$changes" ]; then
+                continue;
+            fi
+            ../scripts/update.sh $1 $(git rev-parse HEAD)
+        fi
+    fi
 done
 
 if [ "$revert" == true ]; then
-	git checkout "$local_branch"
+    git checkout "$local_branch"
 fi

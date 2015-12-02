75002cf7490f13a441daad886dade7e8734b7589
Roland Leißa <leissa@cs.uni-saarland.de>
2015-12-02 15:31:32 +0100

    polish setup_vim_projects.sh script

--- scripts/setup_vim_projects.sh
+++ scripts/setup_vim_projects.sh
@@ -2,5 +2,17 @@
 set -eu
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cp ../vim/.project.vim ../impala/src/
-cp ../vim/.project.vim ../thorin/src/
+PROJECT_VIM="${DIR}"/../vim/.project.vim
+
+echo content of \".project.vim\":
+cat $PROJECT_VIM
+echo
+
+ln -sv $PROJECT_VIM "${DIR}"/../impala/src/
+ln -sv $PROJECT_VIM "${DIR}"/../thorin/src/
+
+echo
+echo "include this in your \"~¸vimrc\" to automatically source the \".project.vim\":"
+echo "if filereadable(\".project.vim\")"
+echo "    source .project.vim"
+echo "endif"

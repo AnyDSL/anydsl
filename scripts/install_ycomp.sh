3b5ca81696c750aa0446bf41add1c2e889e0bbc5
Roland Leißa <leissa@cs.uni-saarland.de>
2015-12-02 16:01:26 +0100

    added script to install ycomp

---
+++ scripts/install_ycomp.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -eu
+
+function usage {
+    echo "usage: install_ycomp.sh <install_dir>"
+}
+
+if [ "$#" -ne 1 ]; then
+    exit 1
+fi
+
+DIR="$(pwd)/$1"
+
+if [ -d $1 ]; then
+    wget http://pp.ipd.kit.edu/firm/download/yComp-1.3.19.zip -O "${DIR}/ycomp.zip"
+    unzip "${DIR}/ycomp.zip" -d "${DIR}"
+    ycomp="${DIR}/yComp-1.3.19/ycomp"
+    chmod u+x ${ycomp}
+    echo "now put \"${ycomp}\" in your path"
+else
+    echo "$1 is not a directory"
+    usage
+    exit 1
+fi

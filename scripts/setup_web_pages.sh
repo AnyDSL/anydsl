b4ecb43f03f287434f0cb05136a6656b6b0b77dd
Roland Leißa <leissa@cs.uni-saarland.de>
2015-12-03 20:00:45 +0100

    added -v

--- scripts/setup_web_pages.sh
+++ scripts/setup_web_pages.sh
@@ -4,10 +4,9 @@ set -eu
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "${DIR}"/..
 
-
 # fetch sources
 git clone git@github.com:AnyDSL/anydsl.github.io
 git clone https://github.com/AnyDSL/anydsl.wiki.git
 
 # symlink git hook
-ln -s "${DIR}/pre-commit-wiki.hook" "${DIR}/../AnyDSL.wiki.git/.git/hooks/."
+ln -sv "${DIR}/pre-commit-wiki.hook" "${DIR}/../anydsl.wiki/.git/hooks/."

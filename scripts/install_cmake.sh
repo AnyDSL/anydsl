5b4840732897b3e95b1c9ee687b590fcffb516df
Roland Leißa <leissa@cs.uni-saarland.de>
2015-12-03 19:31:57 +0100

    bailing out if $1 is missing

--- scripts/install_cmake.sh
+++ scripts/install_cmake.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
+-set eu
 
-if [ -z "$1" ]
-  then
+if [ -z "$1" ]; then
     echo "Please supply a directory to install cmake!"
+    exit 1
 fi
 
 wget https://cmake.org/files/v3.4/cmake-3.4.1-Linux-x86_64.sh

b4317a15f3774163b0561a7a997ee53706f4a4fb
MasterCassim <MasterCassim@users.noreply.github.com>
2015-12-03 18:56:09 +0100

    Create install_cmake.sh

---
+++ scripts/install_cmake.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if [ -z "$1" ]
+  then
+    echo "Please supply a directory to install cmake!"
+fi
+
+wget https://cmake.org/files/v3.4/cmake-3.4.1-Linux-x86_64.sh
+sed -i '/interactive=TRUE/c\interactive=FALSE' cmake-3.4.1-Linux-x86_64.sh
+chmod +x cmake-3.4.1-Linux-x86_64.sh
+./cmake-3.4.1-Linux-x86_64.sh --prefix=$1
+rm cmake-3.4.1-Linux-x86_64.sh

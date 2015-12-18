8b2538745d3a82438748651733473649d3e98ac8
MasterCassim <MasterCassim@users.noreply.github.com>
2015-12-18 13:58:10 +0100

    Update setup.sh to accommodate  travis

--- setup.sh
+++ setup.sh
@@ -20,22 +20,6 @@ function remote {
     fi
 }
 
-# fetch sources
-wget http://llvm.org/releases/3.4.2/llvm-3.4.2.src.tar.gz
-tar xf llvm-3.4.2.src.tar.gz
-rm llvm-3.4.2.src.tar.gz
-mv llvm-3.4.2.src llvm
-cd llvm/tools
-wget http://llvm.org/releases/3.4.2/cfe-3.4.2.src.tar.gz
-tar xf cfe-3.4.2.src.tar.gz
-rm cfe-3.4.2.src.tar.gz
-mv cfe-3.4.2.src clang
-cd "${CUR}"
-git clone `remote github.com:AnyDSL/thorin.git` -b ${BRANCH}
-git clone `remote github.com:AnyDSL/impala.git` -b ${BRANCH}
-git clone `remote github.com:simoll/libwfv.git`
-git clone --recursive `remote github.com:AnyDSL/stincilla.git`
-
 # create build/install dirs
 mkdir -p llvm_build/
 mkdir -p llvm_install/
@@ -44,10 +28,36 @@ mkdir -p impala/build/
 mkdir -p libwfv/build/
 mkdir -p stincilla/build/
 
-# build llvm
-cd llvm_build
-cmake ../llvm -DLLVM_REQUIRES_RTTI:BOOL=true -DCMAKE_BUILD_TYPE:STRING=${BUILD_TYPE} -DCMAKE_INSTALL_PREFIX:PATH="${CUR}/llvm_install"
-make install -j${THREADS}
+# fetch sources
+if [ "$FETCH_LLVM" = true ] ; then
+    wget http://llvm.org/releases/3.6.2/clang+llvm-3.6.2-x86_64-linux-gnu-ubuntu-14.04.tar.xz
+    tar -xvf clang+llvm-3.6.2-x86_64-linux-gnu-ubuntu-14.04.tar.xz
+    rm clang+llvm-3.6.2-x86_64-linux-gnu-ubuntu-14.04.tar.xz
+    mv clang+llvm-3.6.2-x86_64-linux-gnu-ubuntu-14.04/ llvm_install/
+    
+    find /home/travis/work/anydsl/llvm_install/share/llvm/cmake/ -type f -exec sed -i 's#/home/development/llvm/3.6.2/final/Phase3/Release/llvmCore-3.6.2-final.install/#/home/travis/work/anydsl/llvm_install/#g' {} \;
+else
+    wget http://llvm.org/releases/3.4.2/llvm-3.4.2.src.tar.gz
+    tar xf llvm-3.4.2.src.tar.gz
+    rm llvm-3.4.2.src.tar.gz
+    mv llvm-3.4.2.src llvm
+    cd llvm/tools
+    wget http://llvm.org/releases/3.4.2/cfe-3.4.2.src.tar.gz
+    tar xf cfe-3.4.2.src.tar.gz
+    rm cfe-3.4.2.src.tar.gz
+    mv cfe-3.4.2.src clang
+    
+    # build llvm
+    cd llvm_build
+    cmake ../llvm -DLLVM_REQUIRES_RTTI:BOOL=true -DCMAKE_BUILD_TYPE:STRING=${BUILD_TYPE} -DCMAKE_INSTALL_PREFIX:PATH="${CUR}/llvm_install"
+    make install -j${THREADS}
+fi
+
+cd "${CUR}"
+git clone `remote github.com:AnyDSL/thorin.git` -b ${BRANCH}
+git clone `remote github.com:AnyDSL/impala.git` -b ${BRANCH}
+git clone `remote github.com:simoll/libwfv.git`
+git clone --recursive `remote github.com:AnyDSL/stincilla.git`
 
 # build libwfv
 cd "${CUR}/libwfv/build"

ea0cb90dc748b138a715c86d7e2ae7bc9bf55559
Stefan Lemme <stefan.lemme@dfki.de>
2017-02-22 15:29:40 +0100

    Add Half to dependency setup

--- fetch-prebuilt-dependencies.cmake
+++ fetch-prebuilt-dependencies.cmake
@@ -13,6 +13,29 @@ message(STATUS "PLATFORM: ${PLATFORM}")
 
 include(common.cmake)
 
+# Half
+SET ( HALF_VERSION 1.11.0 )
+SET ( HALF_URL "http://downloads.sourceforge.net/project/half/half/${HALF_VERSION}/half-${HALF_VERSION}.zip" )
+
+FIND_PATH (Half_DIR half.hpp
+    PATHS
+        ${Half_DIR}
+        $ENV{Half_DIR}
+        ${SETUP_DIR}/half-${HALF_VERSION}
+    PATH_SUFFIXES
+        include
+)
+if ( NOT Half_DIR )
+    set(HALF_FILE half.zip)
+    if (NOT EXISTS ${SETUP_DIR}/${HALF_FILE})
+        message(STATUS "Downloading ${HALF_URL}")
+        file(DOWNLOAD ${HALF_URL} ${SETUP_DIR}/${HALF_FILE})
+    endif ()
+    decompress(${HALF_FILE})
+    FIND_PATH (Half_DIR half.hpp PATHS ${SETUP_DIR}/half-${HALF_VERSION} PATH_SUFFIXES include)
+endif ()
+message ( STATUS "Half_DIR: ${Half_DIR}" )
+
 # LLVM and clang
 SET ( LLVM_FILE llvm-3.8.1-${PLATFORM} )
 SET ( LLVM_ARCHIVE ${LLVM_FILE}.zip )

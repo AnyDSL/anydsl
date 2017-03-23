bca886f838b01c15114da051ddd8d03ea7d9fb10
Stefan Lemme <stefan.lemme@dfki.de>
2017-03-23 23:19:04 +0100

    Remove _outvar and use _pkg_path instead, fix lowercase for pkg-config.cmake

--- cmake/fetch-package.cmake
+++ cmake/fetch-package.cmake
@@ -1,25 +1,30 @@
-function(fetch_anydsl_package _outvar _pkg_name _pkg_fullname _pkg_url)
+function(fetch_anydsl_package _pkg_path _pkg_name _pkg_fullname _pkg_url)
     # TODO: make this consistent
     # set(_pkg_fullname "AnyDSL_${_pkg_name}")
     set(_pkg_branch "AnyDSL_${_pkg_name}_BRANCH")
     if(NOT ${_pkg_branch})
         set(${_pkg_branch} ${AnyDSL_DEFAULT_BRANCH} CACHE STRING "follow branch of repository ${_pkg_url}")
     endif()
-    find_path(_pkg_path ${_pkg_fullname}-config.cmake
+    string(TOLOWER ${_pkg_fullname} _pkg_fullname_lower)
+    find_path(${_pkg_path}
+        NAMES ${_pkg_fullname}-config.cmake ${_pkg_fullname_lower}-config.cmake
         PATHS
-            ${${_outvar}}
-            $ENV{${_outvar}}
+            ${${_pkg_path}}
+            $ENV{${_pkg_path}}
             ${CMAKE_CURRENT_SOURCE_DIR}/${_pkg_name}/build
             ${ARGN}
         PATH_SUFFIXES
             share/${_pkg_fullname}/cmake
     )
-    if(NOT _pkg_path AND NOT EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${_pkg_name})
+    if(NOT ${_pkg_path} AND NOT EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${_pkg_name})
         find_package(Git REQUIRED)
         execute_process(COMMAND ${GIT_EXECUTABLE} clone --branch ${${_pkg_branch}} --recursive ${_pkg_url} ${_pkg_name} WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
     endif()
     add_subdirectory(${_pkg_name})
-    find_path(_pkg_path ${_pkg_fullname}-config.cmake PATHS ${${_outvar}} ${CMAKE_CURRENT_BINARY_DIR} ${CMAKE_BINARY_DIR} ${ARGN} PATH_SUFFIXES share/${_pkg_fullname}/cmake)
-    set(${_outvar} ${_pkg_path} CACHE PATH "path to package ${_pkg_fullname}")
-    unset(_pkg_path CACHE)
+    find_path(${_pkg_path}
+        NAMES ${_pkg_fullname}-config.cmake ${_pkg_fullname_lower}-config.cmake
+        PATHS ${${_pkg_path}} ${CMAKE_CURRENT_BINARY_DIR} ${CMAKE_BINARY_DIR} ${ARGN}
+        PATH_SUFFIXES share/${_pkg_fullname}/cmake
+        DOC "path to package ${_pkg_fullname}")
 endfunction()
+

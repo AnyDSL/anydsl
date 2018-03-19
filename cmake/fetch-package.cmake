92d9be83c34ccc73983dd17b2e5ea59624588d15
Stefan Lemme <stefan.lemme@dfki.de>
2018-03-19 15:38:02 +0100

    Add custom targets pull, pull-thorin, pull-impala, pull-runtime

--- cmake/fetch-package.cmake
+++ cmake/fetch-package.cmake
@@ -17,8 +17,8 @@ function(fetch_anydsl_package _pkg_path _pkg_name _pkg_fullname _pkg_url)
             share/${_pkg_fullname}/cmake
             share/anydsl/cmake
     )
+    find_package(Git REQUIRED)
     if(NOT ${_pkg_path} AND NOT EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${_pkg_name})
-        find_package(Git REQUIRED)
         execute_process(COMMAND ${GIT_EXECUTABLE} clone --branch ${${_pkg_branch}} --recursive ${_pkg_url} ${_pkg_name} WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
     endif()
     add_subdirectory(${_pkg_name})
@@ -27,5 +27,13 @@ function(fetch_anydsl_package _pkg_path _pkg_name _pkg_fullname _pkg_url)
         PATHS ${${_pkg_path}} ${CMAKE_CURRENT_BINARY_DIR} ${CMAKE_BINARY_DIR} ${ARGN}
         PATH_SUFFIXES share/${_pkg_fullname}/cmake share/anydsl/cmake
         DOC "path to package ${_pkg_fullname}")
+    add_custom_target(pull-${_pkg_name}
+        COMMAND ${GIT_EXECUTABLE} fetch --tags origin
+        COMMAND ${GIT_EXECUTABLE} checkout ${${_pkg_branch}}
+        COMMAND ${GIT_EXECUTABLE} symbolic-ref HEAD
+        COMMAND ${GIT_EXECUTABLE} pull
+        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/${_pkg_name}
+        COMMENT ">>> pull ${_pkg_url} ${${_pkg_branch}}"
+    )
 endfunction()
 

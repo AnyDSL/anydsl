17e458c37388f805d26fe2d11738f2e35da40fbe
Roland Leißa <leissa@cs.uni-saarland.de>
2020-09-30 17:48:03 +0200

    remove scripts nobody uses anymore

--- scripts/setup_web_pages.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -eu
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd "${DIR}"/..
-
-git clone git@github.com:AnyDSL/anydsl.github.io
--- scripts/setup_wiki.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -eu
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd "${DIR}"/..
-
-git clone https://github.com/AnyDSL/anydsl.wiki.git
-ln -sv "${DIR}/pre-commit-wiki.hook" "${DIR}/../anydsl.wiki/.git/hooks/pre-commit"

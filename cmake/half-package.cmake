021f592cecbce0debfe744c6021f1ec121c7470c
Stefan Lemme <stefan.lemme@dfki.de>
2021-07-14 13:53:05 +0200

    Converge dependency handling using CMake's FetchContent

---
+++ cmake/half-package.cmake
@@ -0,0 +1,25 @@
+
+set(AnyDSL_PKG_Half_VERSION "2.2.0" CACHE STRING "Half version of AnyDSL")
+set(AnyDSL_PKG_Half_URL "https://svn.code.sf.net/p/half/code/tags/release-${AnyDSL_PKG_Half_VERSION}" CACHE STRING "where to download Half")
+
+
+include(FetchContent)
+
+
+FetchContent_Declare(Half
+    SVN_REPOSITORY  ${AnyDSL_PKG_Half_URL}
+)
+
+message(STATUS "Make Half available ...")
+FetchContent_MakeAvailable(Half)
+
+find_path(Half_DIR half.hpp
+    PATHS
+        ${half_SOURCE_DIR}
+        ${half_BINARY_DIR}
+    PATH_SUFFIXES
+        include
+        include/half
+    DOC "C++ library for half precision floating point arithmetics."
+)
+

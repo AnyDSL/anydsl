5f3b6b1ba0e2703441a30911f8043d3d31ab47da
Matthias Kurtenacker <matthias.kurtenacker@dfki.de>
2024-06-17 17:01:54 +0200

    Resurrect the old setup script from master.
    There have been some changes to how RV is being built. To accommodate
    these changes, RV is now built separate from LLVM, and Thorin is being
    pointed to the new build directory RV resides in. Both LLVM and RV are
    being build as shared libraries, so no linking errors should arise from
    this change.

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ COLOR_RED="\033[0;31m"
 COLOR_RESET="\033[0m"
 
 echo ">>> update setup project"
-git fetch
+git fetch origin
 
 UPSTREAM=${1:-'@{u}'}
 LOCAL=$(git rev-parse @)
@@ -16,12 +16,17 @@ if [ $LOCAL = $REMOTE ]; then
     echo "your branch is up-to-date"
 elif [ $LOCAL = $BASE ]; then
     echo "your branch is behind your tracking branch"
-    echo "please update your repository"
+    echo "I pull and rerun the script "
+    git pull
+    ./$0
+    exit $?
 elif [ $REMOTE = $BASE ]; then
     echo "your branch is ahead of your tracking branch"
-    echo "remember to push your changes"
+    echo "remember to push your changes but I will run the script anyway"
 else
     echo "your branch and your tracking remote branch have diverged"
+    echo "resolve all conflicts before rerunning the script"
+    exit 1
 fi
 
 if [ ! -e config.sh ]; then
@@ -44,13 +49,18 @@ function remote {
 }
 
 function clone_or_update {
+    # $1: github organisation
+    # $2: github repo name
+    # $3: [optional] branch/tag -> defaults to "master"
+    # $4: [optional] directory name into which to checkout the repo -> defaults to $2
     branch=${3:-master}
-    if [ ! -e "$2" ]; then
+    checkoutpath=${4:-$2}
+    if [ ! -e "$checkoutpath" ]; then
         echo ">>> clone $1/$2 $COLOR_RED($branch)$COLOR_RESET"
-        echo -e "git clone --recursive `remote $1/$2.git` --branch $branch"
-        git clone --recursive `remote $1/$2.git` --branch $branch
+        echo -e "git clone --recursive `remote $1/$2.git` --branch $branch $checkoutpath"
+        git clone --recursive `remote $1/$2.git` --branch $branch $checkoutpath
     else
-        cd $2
+        cd $checkoutpath
         echo -e ">>> pull $1/$2 $COLOR_RED($branch)$COLOR_RESET"
         git fetch --tags origin
         git checkout $branch
@@ -62,7 +72,7 @@ function clone_or_update {
         set -e
         cd ..
     fi
-    mkdir -p "$2"/build/
+    mkdir -p "$checkoutpath"/build/
 }
 
 # build custom CMake
@@ -80,98 +90,169 @@ if [ "${CMAKE-}" == true ]; then
     echo $PATH
 fi
 
-if [ "${LLVM_PREBUILD-}" == true ]; then
-    if [ ! -d llvm_install ]; then
-        ./scripts/download_llvm.sh --version ${LLVM_VERSION}
+# fetch sources
+if [ "${LLVM-}" == true ]; then
+    mkdir -p llvm_build/
+    mkdir -p rv_build/
+
+    if [ "${LLVM_GIT-}" = true ]; then
+        clone_or_update ${LLVM_GITHUB_ORG} llvm-project ${LLVM_GIT_BRANCH}
     else
-        echo "remember to download LLVM if a newer build is available."
+        if [ ! -e "${CUR}/llvm-project" ]; then
+            wget https://github.com/llvm/llvm-project/releases/download/llvmorg-${LLVM_SRC_VERSION}/llvm-project-${LLVM_SRC_VERSION}.src.tar.xz
+            tar xf llvm-project-${LLVM_SRC_VERSION}.src.tar.xz
+            rm llvm-project-${LLVM_SRC_VERSION}.src.tar.xz
+            mv llvm-project-${LLVM_SRC_VERSION}.src llvm-project
+        fi
     fi
-    LLVM_EXTERN="${CUR}/llvm_install"
-    : ${RV_MODULE_BUILD:=ON}
-fi
-if [ "${LLVM_EXTERN:-}" != "" ] && [ -d ${LLVM_EXTERN:-} ]; then
-    LLVM_AUTOBUILD=OFF
-elif [ "${LLVM_AUTOBUILD:-}" == "" ]; then
-    LLVM_AUTOBUILD=ON
+    cd llvm-project
+    if ! patch --dry-run --reverse --force -s -p1 -i ../patches/llvm/nvptx_feature.patch; then
+        patch -p1 -i ../patches/llvm/nvptx_feature.patch
+    fi
+
+    # rv
+    if [ "${RV-}" == true ]; then
+        cd "${CUR}"
+        cd llvm-project
+        clone_or_update cdl-saarland rv ${BRANCH_RV}
+        cd rv
+        git submodule update --init
+    fi
+
+    # build llvm
+    cd "${CUR}"
+    cd llvm_build
+    DEFAULT_SYSROOT=
+    RV_REBUILD_GENBC=OFF
+    if [[ ${OSTYPE} == "darwin"* ]]; then
+        DEFAULT_SYSROOT=`xcrun --sdk macosx --show-sdk-path`
+        RV_REBUILD_GENBC=ON
+    fi
+    cmake ../llvm-project/llvm ${CMAKE_MAKE} -DLLVM_BUILD_LLVM_DYLIB:BOOL=ON -DLLVM_LINK_LLVM_DYLIB:BOOL=ON -DCMAKE_BUILD_TYPE:STRING=${BUILD_TYPE} -DCMAKE_INSTALL_PREFIX:PATH="${CUR}/llvm_install" \
+        -DLLVM_ENABLE_RTTI:BOOL=ON -DLLVM_ENABLE_PROJECTS:STRING="clang;lld" -DLLVM_ENABLE_BINDINGS:BOOL=OFF -DLLVM_INCLUDE_TESTS:BOOL=ON -DLLVM_TARGETS_TO_BUILD:STRING="${LLVM_TARGETS}" -DDEFAULT_SYSROOT:PATH="${DEFAULT_SYSROOT}"
+    ${MAKE} install
+    cd "${CUR}"
+
+    LLVM_VARS=-DLLVM_DIR:PATH="${CUR}/llvm_install/lib/cmake/llvm"
+
+    if [ "${RV-}" == true ]; then
+        cd rv_build
+        cmake ../llvm-project/rv ${CMAKE_MAKE} ${LLVM_VARS} -DBUILD_SHARED_LIBS:BOOL=ON -DCMAKE_BUILD_TYPE:STRING=${BUILD_TYPE} -DRV_REBUILD_GENBC:BOOL=${RV_REBUILD_GENBC} -DCMAKE_INSTALL_PREFIX="${CUR}/llvm_install"
+        ${MAKE}
+        RV_VARS=-DRV_DIR:PATH="${CUR}/rv_build/share/anydsl/cmake"
+        cd "${CUR}"
+    else
+        RV_VARS=-DCMAKE_DISABLE_FIND_PACKAGE_RV:BOOL=TRUE
+    fi
+else
+    LLVM_VARS=-DCMAKE_DISABLE_FIND_PACKAGE_LLVM:BOOL=TRUE
+    RV_VARS=-DCMAKE_DISABLE_FIND_PACKAGE_RV:BOOL=TRUE
 fi
 
-mkdir -p build
+if [ ! -e "${CUR}/half" ]; then
+    svn checkout svn://svn.code.sf.net/p/half/code/trunk half
+fi
 
 # source this file to put artic, impala, and clang in path
 cat > "${CUR}/project.sh" <<_EOF_
-export PATH="${CUR}/build/bin:\${PATH:-}"
-export LD_LIBRARY_PATH="${CUR}/build/lib:\${LD_LIBRARY_PATH:-}"
-export LIBRARY_PATH="${CUR}/build/lib:\${LIBRARY_PATH:-}"
-export CMAKE_PREFIX_PATH="${CUR}/build/share/anydsl/cmake:\${CMAKE_PREFIX_PATH:-}"
-export THORIN_RUNTIME_PATH="${CUR}/runtime/platforms"
+export PATH="${CUR}/llvm_install/bin:${CUR}/artic/build/bin:${CUR}/impala/build/bin:\${PATH:-}"
+export LD_LIBRARY_PATH="${CUR}/llvm_install/lib:\${LD_LIBRARY_PATH:-}"
 _EOF_
-
-if [ "${LLVM_EXTERN:-}" != "" ] && [ -d ${LLVM_EXTERN:-} ]; then
-cat >> "${CUR}/project.sh" <<_EOF_
-export PATH="${LLVM_EXTERN}/bin:\${PATH:-}"
-export LD_LIBRARY_PATH="${LLVM_EXTERN}/lib:\${LD_LIBRARY_PATH:-}"
-export LIBRARY_PATH="${LLVM_EXTERN}/lib:\${LIBRARY_PATH:-}"
-export CMAKE_PREFIX_PATH="${LLVM_EXTERN}/lib/cmake/llvm:\${CMAKE_PREFIX_PATH:-}"
-_EOF_
-fi
-
-if [ "${LLVM_AUTOBUILD:-}" == "ON" ] || [ "${LLVM_AUTOBUILD:-}" == "true" ]; then
-cat >> "${CUR}/project.sh" <<_EOF_
-export PATH="${CUR}/build/_deps/llvm-build/bin:\${PATH:-}"
-export LD_LIBRARY_PATH="${CUR}/build/_deps/llvm-build/lib:\${LD_LIBRARY_PATH:-}"
-export LIBRARY_PATH="${CUR}/build/_deps/llvm-build/lib:\${LIBRARY_PATH:-}"
-export CMAKE_PREFIX_PATH="${CUR}/build/_deps/llvm-build/lib/cmake/llvm:\${CMAKE_PREFIX_PATH:-}"
-_EOF_
-fi
-
 if [ "${CMAKE-}" == true ]; then
-cat >> "${CUR}/project.sh" <<_EOF_
-export PATH="${CUR}/cmake_install/bin:\${PATH:-}"
-_EOF_
+    echo "export PATH=\"${CUR}/cmake_install/bin:\${PATH:-}\"" >> ${CUR}/project.sh
 fi
-
-cat >> "${CUR}/project.sh" <<_EOF_
-if [ "\${ZSH_VERSION:-}" != "" ]; then
-    export fpath=(${CUR}/zsh \${fpath:-})
-    compinit
-fi
-alias anydsl-rebuild="ninja -C ${CUR}/build"
-_EOF_
 
 source "${CUR}/project.sh"
 
-cd "${CUR}/applications"
+# PAL
+if [ "${BUILD_PAL-}" == true ]; then
+    cd "${CUR}"
+    PAL_DIR="${CUR}/pal"
+    # only clone PAL once (and don't update) because we're tied to a particular commit
+    if [ ! -e "${PAL_DIR}" ]; then
+        clone_or_update GPUOpen-Drivers pal dev
+        # PAL depends on specific dependencies to be set up
+        cd "${CUR}/pal/src/util/imported"
+        clone_or_update GPUOpen-Drivers MetroHash amd-master pal_metrohash
+        clone_or_update GPUOpen-Drivers CWPack    amd-master pal_cwpack
+        cd "${CUR}/pal"
+        git checkout ${PAL_COMMIT}
+        # make sure PAL fits into AnyDSL's CMake setup
+        git apply "${CUR}/patches/pal/cmake-setup.patch"
+    fi
+    cd "${CUR}/pal/build"
+    cmake .. ${CMAKE_MAKE} -DCMAKE_BUILD_TYPE:STRING=${BUILD_TYPE} -DCMAKE_POSITION_INDEPENDENT_CODE="ON" \
+        -DPAL_CLIENT_INTERFACE_MAJOR_VERSION=777 -DPAL_CLIENT:STRING="Vulkan" -DPAL_BUILD_GFX:STRING="ON" \
+        -DPAL_BUILD_NULL_DEVICE:STRING="ON" -DPAL_BUILD_OSS:STRING="ON" -DPAL_BUILD_GFX9:STRING="ON" \
+        -DPAL_DEVELOPER_BUILD:STRING="ON" -DPAL_BUILD_NAVI12:STRING="ON" -DPAL_BUILD_NAVI14:STRING="ON" \
+        -DPAL_BUILD_NAVI21:STRING="ON" -DPAL_BUILD_NAVI22:STRING="ON" -DPAL_BUILD_NAVI23:STRING="ON" \
+        -DPAL_BUILD_NAVI24:STRING="ON" -DPAL_BUILD_NAVI31:STRING="ON" -DPAL_BUILD_NAVI32:STRING="ON" \
+        -DPAL_BUILD_NAVI33:STRING="ON"
+    ${MAKE}
 
-BUILD_APPLICATIONS=""
-if [ "${CLONE_STINCILLA}" == true ]; then
-    clone_or_update AnyDSL stincilla ${BRANCH_STINCILLA}
-    BUILD_APPLICATIONS="-DAnyDSL_BUILD_stincilla:BOOL=${BUILD_STINCILLA} ${BUILD_APPLICATIONS}"
-fi
-if [ "${CLONE_RODENT}" == true ]; then
-    clone_or_update AnyDSL rodent ${BRANCH_RODENT}
-    BUILD_APPLICATIONS="-DAnyDSL_BUILD_rodent:BOOL=${BUILD_RODENT} ${BUILD_APPLICATIONS}"
+    # ROCm device libs
+    cd "${CUR}"
+    clone_or_update ${LLVM_GITHUB_ORG} ROCm-Device-Libs ${LLVM_GIT_BRANCH} rocm-device-libs
+    cd "${CUR}/rocm-device-libs"
+    if ! patch --dry-run --reverse --force -s -p1 -i ${CUR}/patches/rocm-device-libs/target-triple.patch; then
+        git apply ${CUR}/patches/rocm-device-libs/target-triple.patch
+    fi
+    cd "${CUR}/rocm-device-libs/build"
+    cmake .. -DCMAKE_PREFIX_PATH:PATH=${CUR}/llvm_build -DAMDGPU_TARGET_TRIPLE:STRING="amdgcn-amd-amdpal" -DCLANG_OPTIONS_APPEND:STRING="-mcpu=${ROCm_GPU_GENERATION}"
+    ${MAKE}
+    # change calling convention to amdgpu_gfx for calls to ocml
+    cd "${CUR}/rocm-device-libs/build/amdgcn/bitcode"
+    llvm-dis ocml.bc -o ocml.ll
+    python3 "${CUR}/scripts/rocm-device-libs-cc-patcher.py"
+    PAL_VARS=-Dpal_DIR:PATH="${CUR}/pal/build"
+else
+    PAL_VARS=
 fi
 
-cd "${CUR}/build"
-cmake \
-    ${CMAKE_MAKE} \
-    -DCMAKE_BUILD_TYPE:STRING=${BUILD_TYPE} \
-    -DBUILD_SHARED_LIBS:BOOL=ON \
-    -DRUNTIME_JIT:BOOL=${RUNTIME_JIT} \
-    -DBUILD_TESTING:BOOL=OFF \
-    -DAnyDSL_PKG_Half_AUTOBUILD:BOOL=ON \
-    -DAnyDSL_PKG_LLVM_AUTOBUILD:BOOL=${LLVM_AUTOBUILD} \
-    -DAnyDSL_PKG_RV_AUTOBUILD:BOOL=${RV_MODULE_BUILD} \
-    -DAnyDSL_thorin_BRANCH:STRING=${BRANCH_THORIN} \
-    -DAnyDSL_artic_BRANCH:STRING=${BRANCH_ARTIC} \
-    -DAnyDSL_impala_BRANCH:STRING=${BRANCH_IMPALA} \
-    -DAnyDSL_runtime_BRANCH:STRING=${BRANCH_RUNTIME} \
-    -DAnyDSL_PKG_LLVM_VERSION:STRING=${LLVM_VERSION} \
-    -DLLVM_TARGETS_TO_BUILD:STRING=${LLVM_TARGETS} \
-    -DAnyDSL_PKG_RV_TAG:STRING=${RV_TAG} \
-    ${BUILD_APPLICATIONS} \
-    ${CUR}
+# thorin
+cd "${CUR}"
+clone_or_update AnyDSL thorin ${BRANCH_THORIN}
+cd "${CUR}/thorin/build"
+cmake .. ${CMAKE_MAKE} -DCMAKE_BUILD_TYPE:STRING=${BUILD_TYPE} ${LLVM_VARS} ${RV_VARS} -DTHORIN_PROFILE:BOOL=${THORIN_PROFILE} -DHalf_DIR:PATH="${CUR}/half/include"
 ${MAKE}
+
+# artic
+cd "${CUR}"
+clone_or_update AnyDSL artic ${BRANCH_ARTIC}
+cd "${CUR}/artic/build"
+cmake .. ${CMAKE_MAKE} -DCMAKE_BUILD_TYPE:STRING=${BUILD_TYPE} -DThorin_DIR:PATH="${CUR}/thorin/build/share/anydsl/cmake"
+${MAKE}
+
+# impala
+cd "${CUR}"
+clone_or_update AnyDSL impala ${BRANCH_IMPALA}
+cd "${CUR}/impala/build"
+cmake .. ${CMAKE_MAKE} -DCMAKE_BUILD_TYPE:STRING=${BUILD_TYPE} -DThorin_DIR:PATH="${CUR}/thorin/build/share/anydsl/cmake"
+${MAKE}
+
+# runtime
+cd "${CUR}"
+clone_or_update AnyDSL runtime ${BRANCH_RUNTIME}
+cd "${CUR}/runtime/build"
+cmake .. ${CMAKE_MAKE} -DCMAKE_BUILD_TYPE:STRING=${BUILD_TYPE} -DRUNTIME_JIT:BOOL=${RUNTIME_JIT} -DDEBUG_OUTPUT:BOOL=${RUNTIME_DEBUG_OUTPUT} -DArtic_DIR:PATH="${CUR}/artic/build/share/anydsl/cmake" -DImpala_DIR:PATH="${CUR}/impala/build/share/anydsl/cmake" ${PAL_VARS}
+${MAKE}
+
+# configure stincilla but don't build yet
+cd "${CUR}"
+clone_or_update AnyDSL stincilla ${BRANCH_STINCILLA}
+cd "${CUR}/stincilla/build"
+cmake .. ${CMAKE_MAKE} -DCMAKE_BUILD_TYPE:STRING=${BUILD_TYPE} -DAnyDSL_runtime_DIR:PATH="${CUR}/runtime/build/share/anydsl/cmake" -DBACKEND:STRING="cpu"
+#${MAKE}
+
+# configure rodent but don't build yet
+if [ "$CLONE_RODENT" = true ]; then
+    cd "${CUR}"
+    clone_or_update AnyDSL rodent ${BRANCH_RODENT}
+    cd "${CUR}/rodent/build"
+    cmake .. ${CMAKE_MAKE} -DCMAKE_BUILD_TYPE:STRING=${BUILD_TYPE} -DAnyDSL_runtime_DIR:PATH="${CUR}/runtime/build/share/anydsl/cmake"
+    #${MAKE}
+fi
+
 cd "${CUR}"
 
 echo

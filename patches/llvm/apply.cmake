4a5d34406291911df12caba8bb7df62203d4a79f
Stefan Lemme <stefan.lemme@dfki.de>
2024-06-21 16:19:46 +0200

    fix LLVM patch script

--- patches/llvm/apply.cmake
+++ patches/llvm/apply.cmake
@@ -7,5 +7,5 @@ set(PATCH_COMMAND ${GIT_EXECUTABLE} apply --ignore-space-change --ignore-whitesp
 
 # all LLVM versions
 execute_process(
-	COMMAND ${PATCH_COMMAND} ${CMAKE_CURRENT_LIST_DIR}/nvptx_feature.patch
+	COMMAND ${PATCH_COMMAND} --directory llvm ${CMAKE_CURRENT_LIST_DIR}/nvptx_feature.patch
 )

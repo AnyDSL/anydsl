576b88d3106bb9b1617744d5a7f14190e43b3429
Matthias Kurtenacker <matthias.kurtenacker@dfki.de>
2024-06-24 19:42:55 +0200

    Improved handling for half, it is required, and llvm, where we should warn users if LLVM cannot be found or when there is a version missmatch.

--- cmake/half-package.cmake
+++ cmake/half-package.cmake
@@ -1,28 +1,36 @@
-
 set(AnyDSL_PKG_Half_VERSION "2.2.0" CACHE STRING "Half version of AnyDSL")
-set(AnyDSL_PKG_Half_URL "https://svn.code.sf.net/p/half/code/tags/release-${AnyDSL_PKG_Half_VERSION}" CACHE STRING "where to download Half")
+set(AnyDSL_PKG_Half_URL "https://sourceforge.net/projects/half/files/half/${AnyDSL_PKG_Half_VERSION}/half-${AnyDSL_PKG_Half_VERSION}.zip/download" CACHE STRING "where to download Half")
 
+find_package(Half QUIET)
+if(((NOT Half_FOUND) OR AnyDSL_PKG_Half_AUTOBUILD) AND NOT CMAKE_DISABLE_FIND_PACKAGE_Half)
+    if (NOT AnyDSL_PKG_Half_AUTOBUILD)
+        message(WARNING
+"AnyDSL_PKG_Half_AUTOBUILD was set to OFF, but CMake could not find Half.
+We will therefore download it anyways.
+To get rid of this warning, either set Half_DIR or enable AnyDSL_PKG_Half_AUTOBUILD.")
+    endif()
 
-include(FetchContent)
+    include(FetchContent)
 
+    FetchContent_Declare(Half
+        URL ${AnyDSL_PKG_Half_URL}
+        DOWNLOAD_NAME half.zip
+        DOWNLOAD_EXTRACT_TIMESTAMP OFF
+    )
 
-FetchContent_Declare(Half
-    URL https://sourceforge.net/projects/half/files/latest/download
-    DOWNLOAD_NAME half.zip
-)
+    message(STATUS "Make Half available ...")
+    FetchContent_GetProperties(Half)
+    if(NOT half_POPULATED)
+        FetchContent_Populate(Half)
+    endif()
 
-message(STATUS "Make Half available ...")
-FetchContent_GetProperties(Half)
-if(NOT half_POPULATED)
-    FetchContent_Populate(Half)
+    find_path(Half_DIR half.hpp
+        PATHS
+            ${half_SOURCE_DIR}
+            ${half_BINARY_DIR}
+        PATH_SUFFIXES
+            include
+            include/half
+        DOC "C++ library for half precision floating point arithmetics."
+    )
 endif()
-
-find_path(Half_DIR half.hpp
-    PATHS
-        ${half_SOURCE_DIR}
-        ${half_BINARY_DIR}
-    PATH_SUFFIXES
-        include
-        include/half
-    DOC "C++ library for half precision floating point arithmetics."
-)

05dadd4de7cf481de47a8934e5ce19308b48db31
Stefan Lemme <stefan.lemme@dfki.de>
2024-11-29 09:45:51 +0100

    update CMake to LLVM 18

--- cmake/llvm-package.cmake
+++ cmake/llvm-package.cmake
@@ -1,17 +1,23 @@
-set(AnyDSL_PKG_LLVM_VERSION "16.0.6" CACHE STRING "LLVM version of AnyDSL")
+set(AnyDSL_PKG_LLVM_VERSION "18.1.8" CACHE STRING "LLVM version of AnyDSL")
 set(AnyDSL_PKG_LLVM_URL "https://github.com/llvm/llvm-project/releases/download/llvmorg-${AnyDSL_PKG_LLVM_VERSION}/llvm-project-${AnyDSL_PKG_LLVM_VERSION}.src.tar.xz" CACHE STRING "where to download LLVM")
 
-
-find_package(LLVM ${AnyDSL_PKG_LLVM_VERSION} QUIET CONFIG)
+# LLVM's version handling requires exact matches of major.minor to ensure API compatibility
+# however, LLVM does not support version ranges for CMake's find_package() command
+string(REGEX MATCH "^([0-9]+)\.([0-9]+)" AnyDSL_PKG_LLVM_VERSION_MAJOR_MINOR ${AnyDSL_PKG_LLVM_VERSION})
+set(AnyDSL_PKG_LLVM_VERSION_MAJOR ${CMAKE_MATCH_1})
+set(AnyDSL_PKG_LLVM_VERSION_MINOR ${CMAKE_MATCH_2})
+foreach(_minor_version RANGE 0 ${AnyDSL_PKG_LLVM_VERSION_MINOR})
+    find_package(LLVM ${AnyDSL_PKG_LLVM_VERSION_MAJOR}.${_minor_version} CONFIG QUIET)
+endforeach()
 if (NOT LLVM_FOUND AND NOT CMAKE_DISABLE_FIND_PACKAGE_LLVM)
-    find_package(LLVM QUIET CONFIG)
+	find_package(LLVM CONFIG QUIET)
     if (NOT LLVM_FOUND)
         message(WARNING
 "LLVM not found. This is probably not what you want to do. You can either set AnyDSL_PKG_LLVM_AUTOBUILD to ON, or set LLVM_DIR to point to LLVM ${AnyDSL_PKG_LLVM_VERSION}.
 You can get also rid of this warning by setting CMAKE_DISABLE_FIND_PACKAGE_LLVM to ON.")
     else()
         message(WARNING
-"LLVM ${LLVM_VERSION} found, but this version does not match what AnyDSL expects. This is probably not what you want to do. You can either set AnyDSL_PKG_LLVM_AUTOBUILD to ON, or set LLVM_DIR to point to LLVM ${AnyDSL_PKG_LLVM_VERSION}.
+		"LLVM ${LLVM_VERSION} found, but this version does not match what AnyDSL expects. This is probably not what you want to do. You can either set AnyDSL_PKG_LLVM_AUTOBUILD to ON, or set LLVM_DIR to point to LLVM ${AnyDSL_PKG_LLVM_VERSION_MAJOR}.
 You can also get rid of this warning by setting AnyDSL_PKG_LLVM_VERSION to ${LLVM_VERSION}, or by enabling CMAKE_DISABLE_FIND_PACKAGE_LLVM.")
     endif()
 endif()

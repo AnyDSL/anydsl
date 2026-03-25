feabc739922b07b2c6c30605a5dd6443f4361ea4
Richard Membarth <richard.membarth@thi.de>
2026-03-25 11:42:32 +0100

    Update to ROCm 7.1
    - update ROCm dependencies
    - update to latest PAL
    - use dedicated build / install directory for dependencies
    - streamline setup.sh

--- setup.sh
+++ setup.sh
@@ -72,28 +72,27 @@ function clone_or_update {
         set -e
         cd ..
     fi
-    mkdir -p "$checkoutpath"/build/
 }
 
 # build custom CMake
 if [ "${CMAKE-}" == true ]; then
-    mkdir -p cmake_build
-
+    cd "${CUR}"
     clone_or_update Kitware CMake ${BRANCH_CMAKE}
 
-    cd cmake_build
-    cmake ../CMake -DBUILD_CursesDialog:BOOL=ON -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_INSTALL_PREFIX:PATH="${CUR}/cmake_install"
+    mkdir -p "${CUR}/build/cmake"
+    cd "${CUR}/build/cmake"
+    cmake "${CUR}/CMake" ${CMAKE_MAKE} -DBUILD_CursesDialog:BOOL=ON -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_INSTALL_PREFIX:PATH="${CUR}/install/cmake"
     ${MAKE} install
     cd "${CUR}"
 
-    export PATH="${CUR}/cmake_install/bin:${PATH}"
+    export PATH="${CUR}/install/cmake/bin:${PATH}"
     echo $PATH
 fi
 
 # fetch sources
 if [ "${LLVM-}" == true ]; then
-    mkdir -p llvm_build/
-    mkdir -p rv_build/
+    mkdir -p "${CUR}/build/llvm"
+    mkdir -p "${CUR}/build/rv"
 
     if [ "${LLVM_GIT-}" = true ]; then
         clone_or_update ${LLVM_GITHUB_ORG} llvm-project ${LLVM_GIT_BRANCH}
@@ -105,41 +104,39 @@ if [ "${LLVM-}" == true ]; then
             mv llvm-project-${LLVM_SRC_VERSION}.src llvm-project
         fi
     fi
-    cd llvm-project
-    if ! patch --dry-run --reverse --force -s -p1 -i ../patches/llvm/nvptx_feature.patch; then
-        patch -p1 -i ../patches/llvm/nvptx_feature.patch
+    cd "${CUR}/llvm-project"
+    if ! patch --dry-run --reverse --force -s -p1 -i "${CUR}/patches/llvm/nvptx_feature.patch"; then
+        patch -p1 -l -i "${CUR}/patches/llvm/nvptx_feature.patch"
     fi
 
     # rv
     if [ "${RV-}" == true ]; then
-        cd "${CUR}"
-        cd llvm-project
+        cd "${CUR}/llvm-project"
         clone_or_update cdl-saarland rv ${BRANCH_RV}
-        cd rv
+        cd "${CUR}/llvm-project/rv"
         git submodule update --init
     fi
 
     # build llvm
-    cd "${CUR}"
-    cd llvm_build
+    cd "${CUR}/build/llvm"
     DEFAULT_SYSROOT=
     RV_REBUILD_GENBC=OFF
     if [[ ${OSTYPE} == "darwin"* ]]; then
         DEFAULT_SYSROOT=`xcrun --sdk macosx --show-sdk-path`
         RV_REBUILD_GENBC=ON
     fi
-    cmake ../llvm-project/llvm ${CMAKE_MAKE} -DLLVM_BUILD_LLVM_DYLIB:BOOL=ON -DLLVM_LINK_LLVM_DYLIB:BOOL=ON -DCMAKE_BUILD_TYPE:STRING=${BUILD_TYPE} -DCMAKE_INSTALL_PREFIX:PATH="${CUR}/llvm_install" \
+    cmake "${CUR}/llvm-project/llvm" ${CMAKE_MAKE} -DLLVM_BUILD_LLVM_DYLIB:BOOL=ON -DLLVM_LINK_LLVM_DYLIB:BOOL=ON -DCMAKE_BUILD_TYPE:STRING=${BUILD_TYPE} -DCMAKE_INSTALL_PREFIX:PATH="${CUR}/install/llvm" \
         -DLLVM_ENABLE_PROJECTS:STRING="clang;lld" -DLLVM_ENABLE_BINDINGS:BOOL=OFF -DLLVM_INCLUDE_TESTS:BOOL=ON -DLLVM_TARGETS_TO_BUILD:STRING="${LLVM_TARGETS}" -DDEFAULT_SYSROOT:PATH="${DEFAULT_SYSROOT}"
     ${MAKE} install
     cd "${CUR}"
 
-    LLVM_VARS=-DLLVM_DIR:PATH="${CUR}/llvm_install/lib/cmake/llvm"
+    LLVM_VARS=-DLLVM_DIR:PATH="${CUR}/install/llvm/lib/cmake/llvm"
 
     if [ "${RV-}" == true ]; then
-        cd rv_build
-        cmake ../llvm-project/rv ${CMAKE_MAKE} ${LLVM_VARS} -DBUILD_SHARED_LIBS:BOOL=ON -DCMAKE_BUILD_TYPE:STRING=${BUILD_TYPE} -DRV_REBUILD_GENBC:BOOL=${RV_REBUILD_GENBC} -DCMAKE_INSTALL_PREFIX="${CUR}/llvm_install" -DDEFAULT_SYSROOT:PATH="${DEFAULT_SYSROOT}"
+        cd "${CUR}/build/rv"
+        cmake "${CUR}/llvm-project/rv" ${CMAKE_MAKE} ${LLVM_VARS} -DBUILD_SHARED_LIBS:BOOL=ON -DCMAKE_BUILD_TYPE:STRING=${BUILD_TYPE} -DRV_REBUILD_GENBC:BOOL=${RV_REBUILD_GENBC} -DCMAKE_INSTALL_PREFIX="${CUR}/install/llvm" -DDEFAULT_SYSROOT:PATH="${DEFAULT_SYSROOT}"
         ${MAKE}
-        RV_VARS=-DRV_DIR:PATH="${CUR}/rv_build/share/anydsl/cmake"
+        RV_VARS=-DRV_DIR:PATH="${CUR}/build/rv/share/anydsl/cmake"
         cd "${CUR}"
     else
         RV_VARS=-DCMAKE_DISABLE_FIND_PACKAGE_RV:BOOL=TRUE
@@ -150,8 +147,8 @@ else
 fi
 
 if [ ! -e "${CUR}/half" ]; then
-    mkdir half
-    cd half
+    mkdir "${CUR}/half"
+    cd "${CUR}/half"
     wget https://sourceforge.net/projects/half/files/latest/download -O half.zip
     unzip half.zip
     rm half.zip
@@ -160,11 +157,11 @@ fi
 
 # source this file to put artic, impala, and clang in path
 cat > "${CUR}/project.sh" <<_EOF_
-export PATH="${CUR}/llvm_install/bin:${CUR}/artic/build/bin:${CUR}/impala/build/bin:\${PATH:-}"
-export LD_LIBRARY_PATH="${CUR}/llvm_install/lib:\${LD_LIBRARY_PATH:-}"
+export PATH="${CUR}/install/llvm/bin:${CUR}/artic/build/bin:${CUR}/impala/build/bin:\${PATH:-}"
+export LD_LIBRARY_PATH="${CUR}/install/llvm/lib:\${LD_LIBRARY_PATH:-}"
 _EOF_
 if [ "${CMAKE-}" == true ]; then
-    echo "export PATH=\"${CUR}/cmake_install/bin:\${PATH:-}\"" >> ${CUR}/project.sh
+    echo "export PATH=\"${CUR}/install/cmake/bin:\${PATH:-}\"" >> ${CUR}/project.sh
 fi
 
 source "${CUR}/project.sh"
@@ -185,31 +182,29 @@ if [ "${BUILD_PAL-}" == true ]; then
         # make sure PAL fits into AnyDSL's CMake setup
         git apply "${CUR}/patches/pal/cmake-setup.patch"
     fi
-    cd "${CUR}/pal/build"
-    cmake .. ${CMAKE_MAKE} -DCMAKE_BUILD_TYPE:STRING=${BUILD_TYPE} -DCMAKE_POSITION_INDEPENDENT_CODE="ON" \
-        -DPAL_CLIENT_INTERFACE_MAJOR_VERSION=777 -DPAL_CLIENT:STRING="Vulkan" -DPAL_BUILD_GFX:STRING="ON" \
-        -DPAL_BUILD_NULL_DEVICE:STRING="ON" -DPAL_BUILD_OSS:STRING="ON" -DPAL_BUILD_GFX9:STRING="ON" \
-        -DPAL_DEVELOPER_BUILD:STRING="ON" -DPAL_BUILD_NAVI12:STRING="ON" -DPAL_BUILD_NAVI14:STRING="ON" \
-        -DPAL_BUILD_NAVI21:STRING="ON" -DPAL_BUILD_NAVI22:STRING="ON" -DPAL_BUILD_NAVI23:STRING="ON" \
-        -DPAL_BUILD_NAVI24:STRING="ON" -DPAL_BUILD_NAVI31:STRING="ON" -DPAL_BUILD_NAVI32:STRING="ON" \
-        -DPAL_BUILD_NAVI33:STRING="ON"
+    mkdir -p "${CUR}/build/pal"
+    cd "${CUR}/build/pal"
+    cmake "${CUR}/pal" ${CMAKE_MAKE} -DCMAKE_BUILD_TYPE:STRING=${BUILD_TYPE} -DCMAKE_POSITION_INDEPENDENT_CODE="ON" \
+        -DPAL_CLIENT_INTERFACE_MAJOR_VERSION=925 -DPAL_CLIENT:STRING="Vulkan" -DPAL_BUILD_GFX:STRING="ON" \
+        -DPAL_BUILD_NULL_DEVICE:STRING="ON" -DPAL_BUILD_OSS:STRING="ON" -DPAL_BUILD_GFX9:STRING="ON" -DPAL_BUILD_GFX11:STRING="ON" -DPAL_BUILD_GFX12:STRING="ON" \
+        -DPAL_BUILD_HAWK_POINT1:STRING="ON" -DPAL_BUILD_HAWK_POINT2:STRING="ON" -DPAL_BUILD_NAVI48:STRING="ON" -DPAL_BUILD_STRIX_HALO:STRING="ON" \
+        -DPAL_DEVELOPER_BUILD:STRING="ON" -DPAL_AMDGPU_BUILD:STRING="ON" -DPAL_BUILD_RDF:STRING="ON"
     ${MAKE}
 
     # ROCm device libs
-    cd "${CUR}"
-    clone_or_update ${LLVM_GITHUB_ORG} ROCm-Device-Libs ${LLVM_GIT_BRANCH} rocm-device-libs
-    cd "${CUR}/rocm-device-libs"
-    if ! patch --dry-run --reverse --force -s -p1 -i ${CUR}/patches/rocm-device-libs/target-triple.patch; then
-        git apply ${CUR}/patches/rocm-device-libs/target-triple.patch
+    cd "${CUR}/llvm-project"
+    if ! patch --dry-run --reverse --force -s -p1 -i "${CUR}/patches/rocm-device-libs/target-triple.patch"; then
+        git apply "${CUR}/patches/rocm-device-libs/target-triple.patch"
     fi
-    cd "${CUR}/rocm-device-libs/build"
-    cmake .. -DCMAKE_PREFIX_PATH:PATH=${CUR}/llvm_build -DAMDGPU_TARGET_TRIPLE:STRING="amdgcn-amd-amdpal" -DCLANG_OPTIONS_APPEND:STRING="-mcpu=${ROCm_GPU_GENERATION}"
+    mkdir -p "${CUR}/build/rocm-device-libs"
+    cd "${CUR}/build/rocm-device-libs"
+    cmake "${CUR}/llvm-project/amd/device-libs" ${CMAKE_MAKE} -DCMAKE_PREFIX_PATH:PATH="${CUR}/build/llvm" -DAMDGPU_TARGET_TRIPLE:STRING="amdgcn-amd-amdpal" -DCLANG_OPTIONS_APPEND:STRING="-mcpu=${ROCm_GPU_GENERATION}"
     ${MAKE}
     # change calling convention to amdgpu_gfx for calls to ocml
-    cd "${CUR}/rocm-device-libs/build/amdgcn/bitcode"
-    llvm-dis ocml.bc -o ocml.ll
+    cd "${CUR}/build/rocm-device-libs/amdgcn/bitcode"
+    "${CUR}/install/llvm/bin/llvm-dis" ocml.bc -o ocml.ll
     python3 "${CUR}/scripts/rocm-device-libs-cc-patcher.py"
-    PAL_VARS=-Dpal_DIR:PATH="${CUR}/pal/build"
+    PAL_VARS=-Dpal_DIR:PATH="${CUR}/build/pal"
 else
     PAL_VARS=
 fi
@@ -217,6 +212,7 @@ fi
 # thorin
 cd "${CUR}"
 clone_or_update AnyDSL thorin ${BRANCH_THORIN}
+mkdir -p "${CUR}/thorin/build"
 cd "${CUR}/thorin/build"
 cmake .. ${CMAKE_MAKE} -DCMAKE_BUILD_TYPE:STRING=${BUILD_TYPE} ${LLVM_VARS} ${RV_VARS} -DTHORIN_PROFILE:BOOL=${THORIN_PROFILE} -DHalf_DIR:PATH="${CUR}/half/include"
 ${MAKE}
@@ -224,6 +220,7 @@ ${MAKE}
 # artic
 cd "${CUR}"
 clone_or_update AnyDSL artic ${BRANCH_ARTIC}
+mkdir -p "${CUR}/artic/build"
 cd "${CUR}/artic/build"
 cmake .. ${CMAKE_MAKE} -DCMAKE_BUILD_TYPE:STRING=${BUILD_TYPE} -DThorin_DIR:PATH="${CUR}/thorin/build/share/anydsl/cmake"
 ${MAKE}
@@ -231,6 +228,7 @@ ${MAKE}
 # impala
 cd "${CUR}"
 clone_or_update AnyDSL impala ${BRANCH_IMPALA}
+mkdir -p "${CUR}/impala/build"
 cd "${CUR}/impala/build"
 cmake .. ${CMAKE_MAKE} -DCMAKE_BUILD_TYPE:STRING=${BUILD_TYPE} -DThorin_DIR:PATH="${CUR}/thorin/build/share/anydsl/cmake"
 ${MAKE}
@@ -238,6 +236,7 @@ ${MAKE}
 # runtime
 cd "${CUR}"
 clone_or_update AnyDSL runtime ${BRANCH_RUNTIME}
+mkdir -p "${CUR}/runtime/build"
 cd "${CUR}/runtime/build"
 cmake .. ${CMAKE_MAKE} -DCMAKE_BUILD_TYPE:STRING=${BUILD_TYPE} -DRUNTIME_JIT:BOOL=${RUNTIME_JIT} -DDEBUG_OUTPUT:BOOL=${RUNTIME_DEBUG_OUTPUT} -DArtic_DIR:PATH="${CUR}/artic/build/share/anydsl/cmake" -DImpala_DIR:PATH="${CUR}/impala/build/share/anydsl/cmake" ${PAL_VARS}
 ${MAKE}
@@ -245,6 +244,7 @@ ${MAKE}
 # configure stincilla but don't build yet
 cd "${CUR}"
 clone_or_update AnyDSL stincilla ${BRANCH_STINCILLA}
+mkdir -p "${CUR}/stincilla/build"
 cd "${CUR}/stincilla/build"
 cmake .. ${CMAKE_MAKE} -DCMAKE_BUILD_TYPE:STRING=${BUILD_TYPE} -DAnyDSL_runtime_DIR:PATH="${CUR}/runtime/build/share/anydsl/cmake" -DBACKEND:STRING="cpu"
 #${MAKE}
@@ -253,6 +253,7 @@ cmake .. ${CMAKE_MAKE} -DCMAKE_BUILD_TYPE:STRING=${BUILD_TYPE} -DAnyDSL_runtime_
 if [ "$CLONE_RODENT" = true ]; then
     cd "${CUR}"
     clone_or_update AnyDSL rodent ${BRANCH_RODENT}
+    mkdir -p "${CUR}/rodent/build"
     cd "${CUR}/rodent/build"
     cmake .. ${CMAKE_MAKE} -DCMAKE_BUILD_TYPE:STRING=${BUILD_TYPE} -DAnyDSL_runtime_DIR:PATH="${CUR}/runtime/build/share/anydsl/cmake"
     #${MAKE}
